f6cc60d8d326fbf43291aa8b5b9b9cd75b946faa
Ayu15sh <ayu15shz@gmail.com>
2025-06-21 11:37:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Card from './components/Card'
 
-
-
 function App() {
-  
-
   return (
     <>
-    <Navbar/>
-    <section className='h-20'></section>
-    <div className="flex  gap-4 justify-center items-center mt-50 flex-col">
-     
-      <h1 className='text-4xl font-bold bg-gradient-to-r from-yellow-400 via-orange-500 to-pink-500 bg-clip-text text-transparent'>
-        Hello, Ayush
-      </h1>
-      <p className='text-2xl text-lgrey'>What are the tasks for today? </p>
-     
-     <Card/>
-    </div>
-      
+      <Navbar />
+      <section className='h-10 sm:h-20'></section>
+
+      <div className="flex flex-col items-center justify-center px-4 sm:px-8 gap-6">
+        <h1 className='text-3xl sm:text-4xl font-bold text-center bg-gradient-to-r from-yellow-400 via-orange-500 to-pink-500 bg-clip-text text-transparent'>
+          Hello, Ayush
+        </h1>
+        <p className='text-lg sm:text-2xl text-lgrey text-center'>
+          What are the tasks for today?
+        </p>
+
+        <Card />
+      </div>
     </>
   )
 }
